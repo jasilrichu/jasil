9440bc64e57b4f33fc69a80e607bc62e63d3842b
jasilrichu <jasilrichu226@gmail.com>
2025-04-09 13:03:52 +0530

    Create cprogram.c

---
+++ cprogram.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int A[20],i,key,n;
+printf("enter the size of an array");
+  scanf("%d",&n);
+for(i=0;i<=n,i++)
+  {
+printf("enter the elements");
+scanf("%d",&A[i]);
+}
+printf("enter the element to search");
+for(i=0;
